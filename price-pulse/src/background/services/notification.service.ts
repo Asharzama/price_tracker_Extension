0d5748bf0a5431f9decb57cd7faa9e416fe353d0
ashar khan <ashar.khan@pixonix.tech>
2026-08-24 18:26:32 +0400

    resolved issue with notification service

--- price-pulse/src/background/services/notification.service.ts
+++ price-pulse/src/background/services/notification.service.ts
@@ -1,6 +1,26 @@
 export class NotificationService {
-  static async showPriceDrop(productUrl: string): Promise<void> {
+  static async showPriceDrop(
+    title: string,
+    oldPrice: number,
+    newPrice: number,
+    newDisplayPrice: string,
+    productUrl: string,
+  ): Promise<void> {
+    const percentageDrop = ((oldPrice - newPrice) / oldPrice) * 100;
+
     const notificationId = `price-drop-${crypto.randomUUID()}`;
+
+    const createdId = await chrome.notifications.create(notificationId, {
+      type: "basic",
+      iconUrl: chrome.runtime.getURL("icons/images.png"),
+      title: "PricePulse 🔔",
+      message:
+        `${title}\n` +
+        `Now ${newDisplayPrice} — ` +
+        `${percentageDrop.toFixed(1)}% price drop`,
+    });
+
+    console.log("✅ Notification created:", createdId);
 
     await chrome.storage.local.set({
       [`notification:${notificationId}`]: productUrl,
